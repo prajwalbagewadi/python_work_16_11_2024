0bdcb83faf9926c2c26561552e1bd26084cd3d47
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2025-02-28 08:58:06 +0530

    Update lists.py

--- lists.py
+++ lists.py
@@ -33,3 +33,22 @@ print(f"li1={li1}")
 #list countings
 print(f"li1 count={li1.count("jdm1")}")
 print(f"li1 len={len(li1)}")
+
+#index
+print(f"li1 5={li1.index(5)}")
+
+#sort 
+unsrt=[5,4,3,2,1]
+unsrt.sort()
+print(f"unsrt ={unsrt}")
+
+#reverse
+re=["a","b","c"]
+re.reverse()
+print(f"re={re}")
+
+#copy
+a=[1,2,4]
+b=[]
+b=a.copy()
+print(f"b={b}")
