6d74c0c016b2b6dd7439250b184bb10c332032d0
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2025-03-01 09:04:42 +0530

    Create filehandling.py

---
+++ filehandling.py
@@ -0,0 +1,23 @@
+from os import write
+
+
+"""
+x -> create new file
+w -> write into file
+r -> read from file
+a ->
+"""
+
+file=open("file.txt","w")
+file.write("helloworld")
+file.close()
+
+file=open("file.txt","r")
+view=file.read()
+file.close()
+
+print(f"view={view}")
+
+file=open("file.txt","a")
+file.write("\n hellopython")
+file.close()
